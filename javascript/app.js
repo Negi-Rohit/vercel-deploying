f66e21e8112093975893d17d1f4092477eb05d5d
Rohit negi <negirohit040@gmail.com>
2020-06-21 15:53:30 +0000

    Initial commit

---
+++ javascript/app.js
@@ -0,0 +1,9 @@
+function showJohn(){
+  document.getElementById("tanya").style.display="none";
+  document.getElementById("john").style.display="flex";
+}
+
+function showTanya(){
+  document.getElementById("john").style.display="none";
+  document.getElementById("tanya").style.display="flex";
+}
